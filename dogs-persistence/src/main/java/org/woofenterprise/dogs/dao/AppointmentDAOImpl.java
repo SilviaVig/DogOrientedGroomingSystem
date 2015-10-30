d04ef18f821a64e1f3d7c0b16828c58dc2a07c04
SilviaVig <silvia.vigasova@gmail.com>
2015-10-30 20:18:04 +0100

    Implemented AppointmentDAO

--- dogs-persistence/src/main/java/org/woofenterprise/dogs/dao/AppointmentDAOImpl.java
+++ dogs-persistence/src/main/java/org/woofenterprise/dogs/dao/AppointmentDAOImpl.java
@@ -1,35 +1,43 @@
 package org.woofenterprise.dogs.dao;
 
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
 import org.woofenterprise.dogs.entity.Appointment;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Repository
 public class AppointmentDAOImpl implements AppointmentDAO {
 
+    @PersistenceContext
+    EntityManager em;
+
     @Override
     public Appointment findById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return em.find(Appointment.class, id);
     }
 
     @Override
     public void create(Appointment appointment) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        em.persist(appointment);
     }
 
     @Override
     public void delete(Appointment appointment) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        em.remove(appointment);
     }
 
     @Override
     public void update(Appointment appointment) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        em.merge(appointment);
     }
 
     @Override
     public List<Appointment> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return em.createQuery("select a from Appointment a", Appointment.class).getResultList();
     }
 
 }
