00705d7322bc8fb140fb86fbb1c7ff78f0e3ace8
SilviaVig <silvia.vigasova@gmail.com>
2015-10-30 20:28:41 +0100

    Added javadoc to DogDAO

--- dogs-persistence/src/main/java/org/woofenterprise/dogs/dao/DogDAO.java
+++ dogs-persistence/src/main/java/org/woofenterprise/dogs/dao/DogDAO.java
@@ -10,13 +10,40 @@ import org.woofenterprise.dogs.entity.Dog;
  */
 public interface DogDAO {
 
+    /**
+     * Returns dog with specified id.
+     *
+     * @param id dogs id
+     * @return dog entity with specified id
+     */
     public Dog findById(Long id);
 
+    /**
+     * Persists dog entity.
+     *
+     * @param d dog to persist
+     */
     public void create(Dog d);
     
+
+    /**
+     * Updates a persisted dog.
+     *
+     * @param d dog to update
+     */
     public void update(Dog d);
 
+    /**
+     * Removes a persisted dog.
+     *
+     * @param d dog to delete
+     */
     public void delete(Dog d);
 
+    /**
+     * Retrieves all persisted dogs.
+     *
+     * @return list of all persisted dogs
+     */
     public List<Dog> findAll();
 }
