e2026f63659a54e5700bc0095899a9f8fe6cc3b8
Michal Bábel <misostc@hotmail.com>
2015-11-26 18:36:42 +0100

    Added test for AppointmentFacade dealing with appointment duration

--- dogs-service/src/test/java/org/woofenterprise/dogs/facade/AppointmentFacadeImplTest.java
+++ dogs-service/src/test/java/org/woofenterprise/dogs/facade/AppointmentFacadeImplTest.java
@@ -1,17 +1,23 @@
 package org.woofenterprise.dogs.facade;
 
+import java.util.List;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.woofenterprice.dogs.dto.AppointmentDTO;
 import org.woofenterprise.dogs.entity.Appointment;
+import org.woofenterprise.dogs.service.AppointmentDurationService;
 import org.woofenterprise.dogs.service.AppointmentService;
 import org.woofenterprise.dogs.service.utils.BaseTestCase;
 import static org.woofenterprise.dogs.service.utils.EntitiesFactory.createAppointment;
+import org.woofenterprise.dogs.utils.Procedure;
 
 /**
  *
@@ -21,6 +27,9 @@ public class AppointmentFacadeImplTest extends BaseTestCase {
 
     @Mock
     private AppointmentService appointmentService;
+    
+    @Mock
+    private AppointmentDurationService durationService;
 
     @InjectMocks
     private AppointmentFacadeImpl appointmentFacade;
@@ -60,4 +69,15 @@ public class AppointmentFacadeImplTest extends BaseTestCase {
         verify(appointmentService).getAllAppointments();
     }
 
+    @Test
+    public void testCalculateDuration() {
+        Appointment appointment = createAppointment();
+        AppointmentDTO appointmentDTO = mapper.map(appointment, AppointmentDTO.class);
+        
+        Long expected = 50L;
+        
+        when( durationService.getDurationForProcedures( (List<Procedure>) any() ) ).thenReturn(expected);        
+        Long duration = appointmentFacade.calculateAppointmentDuration(appointmentDTO);        
+        assertEquals(expected, duration);
+    }
 }
