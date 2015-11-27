73daced99c874873f6232b98904e465877e9ee39
Michal Bábel <misostc@hotmail.com>
2015-11-27 16:04:01 +0100

    Implemented CustomerFacade

--- dogs-service/src/main/java/org/woofenterprise/dogs/facade/CustomerFacadeImpl.java
+++ dogs-service/src/main/java/org/woofenterprise/dogs/facade/CustomerFacadeImpl.java
@@ -1,35 +1,49 @@
 package org.woofenterprise.dogs.facade;
 
 import java.util.Collection;
+import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 import org.woofenterprice.dogs.dto.CustomerDTO;
+import org.woofenterprise.dogs.service.BeanMappingService;
+import org.woofenterprise.dogs.service.CustomerService;
+import org.woofenterprise.dogs.entity.Customer;
 
 @Service
 public class CustomerFacadeImpl implements CustomerFacade {
-
+    
+    @Inject
+    BeanMappingService beanMappingService;
+    
+    @Inject
+    CustomerService customerService;
+    
     @Override
     public CustomerDTO findCustomerById(Long customerId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Customer c = customerService.findCustomerById(customerId);
+        return beanMappingService.map(c, CustomerDTO.class);
     }
 
     @Override
     public CustomerDTO findCustomerByEmail(String email) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Customer c = customerService.findCustomerByEmail(email);
+        return beanMappingService.map(c, CustomerDTO.class);
     }
 
     @Override
     public Long createCustomer(CustomerDTO customerDTO) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Customer c = beanMappingService.map(customerDTO, Customer.class);
+        return customerService.createCustomer(c);
     }
 
     @Override
     public void deleteCustomer(Long customerId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        customerService.deleteCustomer(customerId);
     }
 
     @Override
     public Collection<CustomerDTO> getAllCustomers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Collection<Customer> customers = customerService.getAllCustomers();
+        return beanMappingService.map(customers, CustomerDTO.class);
     }
-
+    
 }
