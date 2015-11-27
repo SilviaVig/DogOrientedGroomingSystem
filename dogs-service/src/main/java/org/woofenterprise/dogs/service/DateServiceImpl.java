1790e14917c8ffc4c05e7be20c756e5f688b9204
SilviaVig <silvia.vigasova@gmail.com>
2015-11-27 19:57:48 +0100

    Added implementation for DateService

--- dogs-service/src/main/java/org/woofenterprise/dogs/service/DateServiceImpl.java
+++ dogs-service/src/main/java/org/woofenterprise/dogs/service/DateServiceImpl.java
@@ -1,18 +1,24 @@
 package org.woofenterprise.dogs.service;
 
+import java.util.Calendar;
 import java.util.Date;
 import org.springframework.stereotype.Service;
 
 /**
  *
- * @author Michal.Babel
+ * @author Silvia Vigasova
  */
 @Service
 public class DateServiceImpl implements DateService {
 
     @Override
     public Date getToday() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR_OF_DAY, 0);
+        today.set(Calendar.MINUTE, 0);
+        today.set(Calendar.SECOND, 0);
+        today.set(Calendar.MILLISECOND, 0);
+        return today.getTime();
     }
 
 }
